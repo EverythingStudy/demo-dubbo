8ac902d9f3675a6a9ce9a74c48a2dc2f26d578d8
EverythingStudy <759499199@qq.com>
2020-01-03 14:58:06 +0800

    20-01-03

---
+++ dubbo_customer/src/test/java/com/example/dubbo_customer/DubboCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dubbo_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
