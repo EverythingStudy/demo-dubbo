d485fd4e172583e6ed0b49a25cde179930df2e94
EverythingStudy <759499199@qq.com>
2020-01-20 13:43:38 +0800

    dubbo_provider_boot

--- dubbo_provider_boot/src/main/java/com/example/service/ReportServiceImpl.java
+++ dubbo_provider_boot/src/main/java/com/example/service/ReportServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.service;
 
 
+import com.example.bean.User;
 import com.example.dao.ReportMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,14 +15,25 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Author cly
  **/
 
-@Service(version = "1.0.0",interfaceClass = reportService.class)
+@Service(version = "1.0.0", interfaceClass = reportService.class)
 public class ReportServiceImpl implements reportService {
+    private final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
     @Autowired
     ReportMapper reportMapper;
+
     @Override
     public int del(int id) {
-        int cout=reportMapper.deleteByPrimaryKey(id);
+        int cout = reportMapper.deleteByPrimaryKey(id);
         System.out.println(cout);
         return cout;
+    }
+
+    @Override
+    public User get() {
+        User user = new User();
+        user.setName("陈林亚");
+        user.setPhone("18601268581");
+        user.setAddress("保定市");
+        return user;
     }
 }
